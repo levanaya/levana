c166bc6feb808b8f6db378b7351d2b2f7eab5ac3
levana <levanaya@163.com>
2022-10-14 18:16:10 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'levanaya', // Usually your GitHub org/user name.
+  organizationName: 'levana', // Usually your GitHub org/user name.
   projectName: 'levana', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
