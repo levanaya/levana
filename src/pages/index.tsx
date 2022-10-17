c3d003115f9639e2eb3b5cbc6f02c2ba36a3f33d
levana <levanaya@163.com>
2022-10-17 13:35:48 +0800

    update path bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/interview">
             博客 →
           </Link>
         </div>
